11e729e1006876bc34d0a5567aa657d6e6ed8b84
Fabio Petzenhauser <fabio894@gmail.com>
2024-10-30 20:17:59 +0100

    Add busy waiting after sleep in periodic task

--- src/recorder/system/periodic_task.py
+++ src/recorder/system/periodic_task.py
@@ -1,9 +1,11 @@
 import threading
 import time
 
+
 class PeriodicTask:
     def __init__(self, interval, function, *args, **kwargs):
         self.interval = interval
+        self.wakeup_tol = 0.004
         self.function = function
         self.args = args
         self.kwargs = kwargs
@@ -13,12 +15,18 @@ class PeriodicTask:
 
     def _run(self):
         while not self.stop_event.is_set():
-            start_time = time.time()
+            start_time = time.perf_counter()
             self.function(*self.args, **self.kwargs)
-            elapsed_time = time.time() - start_time
-            sleep_time = max(0, self.interval - elapsed_time)
+            elapsed_time = time.perf_counter() - start_time
+            sleep_time = max(0, self.interval - self.wakeup_tol - elapsed_time)
+
+            # non-blocking sleep
             time.sleep(sleep_time)
-        
+
+            # busy waiting
+            while (time.perf_counter() - start_time) <= self.interval:
+                time.sleep(0.000001)
+
     def start(self):
         self.thread.start()
 
