5172de66ea1051248b0776f89bbd3dd1570d04cc
Fabio Petzenhauser <fabio894@gmail.com>
2024-11-04 21:11:23 +0100

    Log data to unique folderW

--- src/recorder/camera/camera_service.py
+++ src/recorder/camera/camera_service.py
@@ -1,17 +1,19 @@
 import time
+from pathlib import Path
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder, Quality
 from recorder.system.process import PythonProcess
 
 
 class CameraServiceImplementation:
-    def __init__(self, rec_start_time: float):
+    def __init__(self, rec_start_time: float, logging_directory: str | Path):
         self._picam = Picamera2()
         video_config = self._picam.create_video_configuration(
             controls={"FrameDurationLimits": (40000, 40000)}
         )
         self._picam.configure(video_config)
         self._rec_start_time = rec_start_time
+        self._logging_directory = Path(logging_directory)
         self._time_offset = None
         self._picam.pre_callback = self.get_time_offset
         self._encoder = H264Encoder()
@@ -23,7 +25,10 @@ class CameraServiceImplementation:
 
     def start(self):
         self._picam.start_recording(
-            self._encoder, "test.h264", pts="frame_timestamps.txt", quality=Quality.HIGH
+            self._encoder,
+            str(self._logging_directory / "video.h264"),
+            pts=str(self._logging_directory / "frame_timestamps.txt"),
+            quality=Quality.HIGH,
         )
 
     def stop(self):
@@ -31,21 +36,23 @@ class CameraServiceImplementation:
         self.postprocess()
 
     def postprocess(self):
-        with open("frame_timestamps.txt", "r") as f:
+        with open(self._logging_directory / "frame_timestamps.txt", "r") as f:
             frame_timestamps = [float(line.strip()) for line in f]
 
-        with open("frame_timestamps.txt", "w") as f:
+        with open(self._logging_directory / "frame_timestamps.txt", "w") as f:
             for timestamp in frame_timestamps:
                 f.write(f"{timestamp+self._time_offset}\n")
 
 
 class CameraService(PythonProcess):
-    def __init__(self, start_time: float):
-        super().__init__(start_time)
+    def __init__(self, start_time: float, logging_directory: str | Path):
+        super().__init__(start_time, logging_directory)
 
     @staticmethod
-    def run(stop_event, start_time):
-        service_implementation = CameraServiceImplementation(rec_start_time=start_time)
+    def run(stop_event, start_time, logging_directory):
+        service_implementation = CameraServiceImplementation(
+            rec_start_time=start_time, logging_directory=logging_directory
+        )
         service_implementation.start()
         while not stop_event.is_set():
             time.sleep(0.04)
--- src/recorder/main.py
+++ src/recorder/main.py
@@ -1,13 +1,23 @@
 import time
+from datetime import datetime
+from pathlib import Path
 from recorder.system.runner import ServiceRunner
 from recorder.BNO055.imu_service import IMUService
 from recorder.camera.camera_service import CameraService
 
 
 if __name__ == "__main__":
+    logging_directory = Path("/home/fabio/data") / datetime.now().strftime(
+        "%Y%m%d_%H_%M_%S"
+    )
+    logging_directory.mkdir(parents=True, exist_ok=True)
     start_time = time.monotonic()
     services = []
-    services.append(IMUService(start_time=start_time))
-    services.append(CameraService(start_time=start_time))
+    services.append(
+        IMUService(start_time=start_time, logging_directory=logging_directory)
+    )
+    services.append(
+        CameraService(start_time=start_time, logging_directory=logging_directory)
+    )
     runner = ServiceRunner(services)
     runner.start()
