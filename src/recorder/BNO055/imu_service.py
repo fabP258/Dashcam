9ee5490aab4754ff20732fbe48f4278fff38df0f
Fabio Petzenhauser <fabio894@gmail.com>
2024-12-23 23:42:50 +0100

    Publish IMU data via IPC and struct serialization

--- src/recorder/BNO055/imu_service.py
+++ src/recorder/BNO055/imu_service.py
@@ -1,6 +1,8 @@
 import time
+import struct
 from pathlib import Path
 from typing import List
+from ipc_pyx import PubSocket
 from recorder.mw.service import Service
 from recorder.mw.rate_keeper import RateKeeper
 from recorder.BNO055.lib.bno055 import BNO055
@@ -10,13 +12,15 @@ class IMUService(Service):
     def __init__(self, start_time: float, logging_directory: str | Path):
         self._start_time = start_time
         self._logging_directory = logging_directory
+        self._sensor = BNO055()
+        self._pub_socket = PubSocket(b"/imu")
+        self._msg_fmt = "fffffff"
 
     def run(self, stop_event):
-        sensor = BNO055()
         data = []
         rate_keeper = RateKeeper(100)
         while not stop_event.is_set():
-            data.append(IMUService.read_sensor_data(sensor))
+            data.append(self.read_sensor_data())
             rate_keeper.wait()
         IMUService.write_data(data, self._start_time, self._logging_directory)
 
@@ -33,9 +37,14 @@ class IMUService(Service):
                     f"{t-start_time},{list_to_str(acc_vec)},{list_to_str(gyr_vec)}\n"
                 )
 
-    @staticmethod
-    def read_sensor_data(sensor: BNO055):
+    def read_sensor_data(self):
         timestamp = time.monotonic()
-        acc_vec = sensor.read_acc_data()
-        gyr_vec = sensor.read_gyr_data()
+        acc_vec = self._sensor.read_acc_data()
+        gyr_vec = self._sensor.read_gyr_data()
+
+        # publish message via IPC
+        msg_data = (timestamp, *acc_vec, *gyr_vec)
+        packed_msg_data = struct.pack(self._msg_fmt, *msg_data)
+        self._pub_socket.send(packed_msg_data)
+
         return timestamp, acc_vec, gyr_vec
