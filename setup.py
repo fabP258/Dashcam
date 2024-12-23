ada04f93cc263c60cdf7ff3de361e412af4863f5
Fabio Petzenhauser <fabio894@gmail.com>
2024-12-23 18:30:33 +0100

    Implement MSGQ python API

--- setup.py
+++ setup.py
@@ -1,3 +1,32 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
 
-setup(name="recorder", version="0.0.1", description="Data recording application",author="Fabio Petzenhauser", author_email="fabio894@gmail.com", packages=find_packages(where="src"), package_dir={"": "src"}, install_requires=[], python_requires=">=3.10")
+extensions = [
+    Extension(
+        name="ipc_pyx",
+        sources=[
+            "src/recorder/mw/msgq/ipc.cc",
+            "src/recorder/mw/msgq/ipc_pyx.pyx",
+            "src/recorder/mw/msgq/msgq.cc",
+        ],
+        language="c++",
+        extra_compile_args=["-std=c++11"],
+        include_dirs=[],
+        libraries=[],
+        library_dirs=[],
+    )
+]
+
+setup(
+    name="recorder",
+    version="0.1.1",
+    description="Data recording application",
+    author="Fabio Petzenhauser",
+    author_email="fabio894@gmail.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[],
+    python_requires=">=3.10",
+    ext_modules=cythonize(extensions),
+    zip_safe=False,
+)
