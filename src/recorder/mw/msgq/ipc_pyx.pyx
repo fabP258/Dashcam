feda29b7b19984b5c7104edcc7439e4ff96cfe87
Fabio Petzenhauser <fabio894@gmail.com>
2024-12-23 18:21:37 +0100

    Rename middleware folder

---
+++ src/recorder/mw/msgq/ipc_pyx.pyx
@@ -0,0 +1,58 @@
+from libcpp.string cimport string
+from libcpp cimport bool
+
+cdef extern from "ipc.h":
+  cdef cppclass MSGQMessage:
+    void init(size_t)
+    void init(char *, size_t)
+    void close()
+    size_t getSize()
+    char *getData()
+
+  cdef cppclass MSGQSubSocket:
+    @staticmethod
+    MSGQSubSocket * create(string, bool)
+    MSGQMessage * receive()
+  
+  cdef cppclass MSGQPubSocket:
+    @staticmethod
+    MSGQPubSocket * create(string)
+    int send (char *, size_t)
+
+cdef class SubSocket:
+  cdef MSGQSubSocket * socket
+  cdef bool is_owner
+
+  def __cinit__(self, string endpoint, bool conflate=False):
+    self.socket = MSGQSubSocket.create(endpoint, conflate)
+    self.is_owner = True
+
+    if self.socket == NULL:
+      raise Exception("Failed to create cppSubSocket.")
+
+  def receive(self):
+    msg = self.socket.receive()
+
+    if msg == NULL:
+      return None
+    else:
+      sz = msg.getSize()
+      data = msg.getData()[:sz]
+      del msg
+
+      return data
+
+cdef class PubSocket:
+  cdef MSGQPubSocket * socket
+
+  def __cinit__(self, string endpoint):
+    self.socket = MSGQPubSocket.create(endpoint)
+    if self.socket == NULL:
+      raise Exception("Failed to create cppPubSocket.")
+
+  def send(self, bytes data):
+    length = len(data)
+    r = self.socket.send(<char*>data, length)
+
+    if r != length:
+      raise Exception("Failed to sen message")
